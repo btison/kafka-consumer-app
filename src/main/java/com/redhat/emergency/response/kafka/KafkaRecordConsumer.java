0dcb73912b30c824c0d70b6590e94289560b8aff
Bernard Tison <bernard.tison@gmail.com>
2021-05-02 21:40:15 +0200

    Use Uni rather than CompletionStage

--- src/main/java/com/redhat/emergency/response/kafka/KafkaRecordConsumer.java
+++ src/main/java/com/redhat/emergency/response/kafka/KafkaRecordConsumer.java
@@ -3,10 +3,10 @@ package com.redhat.emergency.response.kafka;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 import javax.enterprise.context.ApplicationScoped;
 
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecord;
 import io.smallrye.reactive.messaging.kafka.IncomingKafkaRecordMetadata;
 import org.apache.kafka.common.header.Headers;
@@ -22,24 +22,26 @@ public class KafkaRecordConsumer {
 
     @Incoming("channel")
     @Acknowledgment(Acknowledgment.Strategy.PRE_PROCESSING)
-    public CompletionStage<Void> process(IncomingKafkaRecord<String, String> message) {
-        return CompletableFuture.runAsync(() -> {
-            Optional<IncomingKafkaRecordMetadata> metadata = message.getMetadata(IncomingKafkaRecordMetadata.class);
-            StringBuilder sb = new StringBuilder();
-            metadata.ifPresent(m -> {
-                Headers headers = m.getHeaders();
-                Arrays.stream(headers.toArray()).forEach(header -> sb.append(header.key()).append(": ").append(convertToString(header.key(), header.value())).append(", "));
-            });
-            String hs = sb.toString();
-            if (hs.length() > 2) {
-                hs = hs.substring(0, hs.length()-2);
-            }
-            log.info("Consumed message from topic '" + message.getTopic() + "', partition '" 
-                + message.getPartition() + "', offset '" + message.getOffset() + "'");
-            log.info("    Headers: " + hs);
-            log.info("    Message key: " + message.getKey());
-            log.info("    Message value: " + message.getPayload());
-        });
+    public Uni<Void> processUni(IncomingKafkaRecord<String, String> message) {
+        return Uni.createFrom().item(message).emitOn(Infrastructure.getDefaultExecutor())
+                .onItem().transform(record -> {
+                    Optional<IncomingKafkaRecordMetadata> metadata = message.getMetadata(IncomingKafkaRecordMetadata.class);
+                    StringBuilder sb = new StringBuilder();
+                    metadata.ifPresent(m -> {
+                        Headers headers = m.getHeaders();
+                        Arrays.stream(headers.toArray()).forEach(header -> sb.append(header.key()).append(": ").append(convertToString(header.key(), header.value())).append(", "));
+                    });
+                    String hs = sb.toString();
+                    if (hs.length() > 2) {
+                        hs = hs.substring(0, hs.length() - 2);
+                    }
+                    log.info("Consumed message from topic '" + message.getTopic() + "', partition '"
+                            + message.getPartition() + "', offset '" + message.getOffset() + "'");
+                    log.info("    Headers: " + hs);
+                    log.info("    Message key: " + message.getKey());
+                    log.info("    Message value: " + message.getPayload());
+                    return null;
+                });
     }
 
     private String convertToString(String key, byte[] bytes) {
